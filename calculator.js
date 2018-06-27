072bf791e90cd18da6b0a5212cd3dd141fe500e0
Taras Burdeniuk <bur.taras@gmail.com>
2018-06-27 16:53:07 +0300

    calc from jsru

---
+++ calculator.js
@@ -0,0 +1,26 @@
+// Напишите функцию-конструктор Calculator, которая создает объект с тремя методами:
+//
+//     Метод read() запрашивает два значения при помощи prompt и запоминает их в свойствах объекта.
+//     Метод sum() возвращает сумму запомненных свойств.
+//     Метод mul() возвращает произведение запомненных свойств.
+
+
+function Calculator() {
+
+    this.read = function () {
+        this.a = +prompt('first number', '0');
+        this.b = +prompt('second number', '0');
+    };
+    this.sum = function () {
+        return this.a + this.b;
+    };
+    this.mul = function () {
+        return this.a * this.b;
+    }
+}
+
+const calculator = new Calculator();
+calculator.read();
+
+alert("Сумма=" + calculator.sum());
+alert("Произведение=" + calculator.mul());
