f87f181743edc98b616d4a8bd5ff2515d10d5c57
Taras Burdeniuk <bur.taras@gmail.com>
2018-07-04 14:30:38 +0300

    ++

---
+++ reverseInt.js
@@ -0,0 +1,19 @@
+// Реализуйте и экспортируйте по умолчанию функцию reverseInt, которая переворачивает цифры в переданном числе и возвращает новое число.
+
+const reverseInt = (number) => {
+
+    let rev = String(number).split('');
+    let result = [];
+
+    for (let i = rev.length; i >= 0; i--) {
+        if (rev[i] === '-') {
+            result.unshift(rev[i]);
+        } else {
+            result.push(rev[i]);
+        }
+    }
+    return result.join('');
+};
+
+console.log(reverseInt(13)); // 31
+console.log(reverseInt(-123)); // -321
