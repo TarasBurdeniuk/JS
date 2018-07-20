803c8b8bc72eff530c57859e873d4d8e958d574a
Taras Burdeniuk <bur.taras@gmail.com>
2018-07-20 14:34:44 +0300

    ++

---
+++ withoutTwoZeros.js
@@ -0,0 +1,24 @@
+// Реализуйте функцию, которая принимает на вход два аргумента - количество нулей и количество единиц, и определяет сколько есть способов размещения этих нулей и единиц так, что бы не было двух нулей идущих подряд.
+//
+//     Например, определим все способы размещения двух нулей и двух единиц. Существует шесть возможных способов размещения: 0011, 0101, 0110, 1001, 1010, 1100. В трех случаях содержится два нуля, идущих подряд: 0011, 1001 и 1100. Вычитаем их из общего числа и получаем три возможных способа: 0101, 0110 и 1010. Ответ - 3.
+
+const withoutTwoZeros = (zero, one) => {
+    function factorial(number) {
+        let fac = 1;
+        for (let i = number; i >= 1; i--) {
+            fac *= i;
+        }
+        return fac;
+    }
+
+    let allVar = factorial(zero + one);
+
+    let withoutRepeat = allVar / ((factorial((one + zero) - zero) * factorial(zero)));
+
+    return (zero === 2) ? withoutRepeat - (zero + one - 1) : withoutRepeat;
+};
+
+console.log(withoutTwoZeros(2, 2)); // 3
+console.log(withoutTwoZeros(1, 1)); // 2
+console.log(withoutTwoZeros(1, 3)); // 4
+console.log(withoutTwoZeros(2, 4)); // 10
